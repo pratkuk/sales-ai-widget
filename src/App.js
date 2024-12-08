24d8d2b992cdc3283d03fc65bdc91b073d437a70
pratkuk <pratyush.kukreja@gmail.com>
2024-12-07 23:00:00 -0800

    Add App component

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { MessageSquare, X, Send, AlertCircle, TrendingUp, Shield, Clock, Mail, FileCheck, ChevronRight } from 'lucide-react';
+
+// Previous component code here - adding full implementation
