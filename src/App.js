dd75f6bcf3caa6ca6b9f687d8c9b4295ffa81001
pratkuk <pratyush.kukreja@gmail.com>
2024-12-08 03:56:01 -0800

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
 import { MessageSquare, X, Send, AlertCircle, TrendingUp, Shield, Clock, Mail, FileCheck, ChevronRight } from 'lucide-react';
 
-// Previous component code here - adding full implementation
+[Previous App.js code here]
+
+export default SalesAIWidget;
